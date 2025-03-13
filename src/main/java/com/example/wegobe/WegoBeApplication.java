cb5b1c466469dffd8b98bea4503a5b175d7f70f2
dami0806 <dami0804@naver.com>
2025-03-13 20:35:10 +0900

    set init

---
+++ src/main/java/com/example/wegobe/WegoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.wegobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WegoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wegobe/WegoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wegobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
