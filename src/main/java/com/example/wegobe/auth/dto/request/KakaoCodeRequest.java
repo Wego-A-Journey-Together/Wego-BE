5bf76c18a7b12e9d93cad7c50531f1b7966823a4
dami0806 <dami0804@naver.com>
2025-03-27 12:36:05 +0900

    refactor: 프론트측 요구에 맞춰 코드 수정

---
+++ src/main/java/com/example/wegobe/auth/dto/request/KakaoCodeRequest.java
@@ -0,0 +1,10 @@
+package com.example.wegobe.auth.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoCodeRequest{
+    private String code;
+}
