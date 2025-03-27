2b5b6b2ef47b3b4791aa24f29da7284dcbfdb478
dami0806 <dami0804@naver.com>
2025-03-27 14:31:12 +0900

    refactor: 프론트측 요구에 맞춰 코드 수정

--- src/main/java/com/example/wegobe/auth/dto/response/LoginResponse.java
+++ src/main/java/com/example/wegobe/auth/dto/response/LoginResponse.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class LoginResponse {
     private String accessToken;
+    private String refreshToken;
     private String email;
     private String nickname;
 }
