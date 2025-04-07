bc2f53b82fad37c87146b90650e17539c1a33532
qhdudedi <boyoung035@gmail.com>
2025-04-07 15:50:31 +0900

    refactor: 내가 작성한 소감 조회 시, 주최자 정보 반환 추가
    - 작성소감/주최자정보/동행정보 반환되도록 수정

--- src/main/java/com/example/wegobe/review/dto/MyReviewResponseDto.java
+++ src/main/java/com/example/wegobe/review/dto/MyReviewResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.wegobe.review.dto;
 
+import com.example.wegobe.gathering.domain.Gathering;
 import com.example.wegobe.gathering.dto.response.GatheringSimpleResponseDto;
+import com.example.wegobe.profile.WriterProfileDto;
 import com.example.wegobe.review.domain.Review;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,17 +12,24 @@ import java.time.format.DateTimeFormatter;
 @Getter
 @Builder
 public class MyReviewResponseDto {
+
     private String createdAt;
     private Integer rating;
     private String content;
+
     private GatheringSimpleResponseDto gathering; // 내가 쓴 리뷰의 대상 동행
 
+    private WriterProfileDto hostProfile;
+
     public static MyReviewResponseDto fromEntity(Review review) {
+        Gathering gathering = review.getGathering();
+
         return MyReviewResponseDto.builder()
                 .createdAt(review.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
                 .rating(review.getRating())
                 .content(review.getContent())
                 .gathering(GatheringSimpleResponseDto.fromEntity(review.getGathering()))
+                .hostProfile(WriterProfileDto.fromEntity(gathering.getCreator()))
                 .build();
     }
 }
